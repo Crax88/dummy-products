846598b107a0ee15cfc5b326f33c90fe499f39cd
Trafilkin N.A <ntrafilkin88@gmail.com>
2026-04-06 14:59:30 +0300

    feat(UI): Добавили компоненты кнопки и  инпута

---
+++ src/shared/ui/kit/Input.tsx
@@ -0,0 +1,37 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  startSlot?: React.ReactNode;
+  endSlot?: React.ReactNode;
+}
+
+export const Input: React.FC<InputProps> = ({
+  label,
+  error,
+  className,
+  startSlot,
+  endSlot,
+  ...props
+}) => {
+  return (
+    <div className='mb-4'>
+      {label && (
+        <label className='block text-sm font-medium text-gray-700 mb-1'>
+          {label}
+        </label>
+      )}
+      <div className='relative'>
+        {startSlot}
+        <input
+          placeholder='*******'
+          className={`w-full pl-10 pr-10 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+            error ? 'border-red-500' : 'border-gray-300'
+          }`}
+          {...props}
+        />
+        {endSlot}
+      </div>
+      {error && <p className='mt-1 text-sm text-red-600'>{error}</p>}
+    </div>
+  );
+};
