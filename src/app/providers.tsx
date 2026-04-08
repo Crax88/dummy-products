39e62105cd8927170b99c31051756a610ab7be6c
Trafilkin N.A <ntrafilkin88@gmail.com>
2026-04-08 12:20:15 +0300

    feat(Providers): Добавили тоасты

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,13 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/shared/api/query-client';
 import type React from 'react';
+import { ToastContainer } from 'react-toastify';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ToastContainer />
+    </QueryClientProvider>
   );
 }
